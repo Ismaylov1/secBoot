da34096c2a87c002038225839aa8a4d2b247fb7e
Ismaylov1 <zakazdoping86@gmail.com>
2020-12-20 17:05:04 +0500

    vb

---
+++ secbootartifact/src/main/java/sec/app/secboot/SecBootAppApplication.java
@@ -0,0 +1,13 @@
+package sec.app.secboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecBootAppApplication.class, args);
+    }
+
+}
---
+++ secbootartifact/src/test/java/sec/app/secboot/SecBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sec.app.secboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
